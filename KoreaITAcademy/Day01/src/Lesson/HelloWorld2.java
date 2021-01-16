0f3cd2e8433ec5e1a5d40525f97b46a5869433ae
MG Song <mg-song@poongsan.co.kr>
2021-01-16 18:23:30 +0900

    string 변수 사용 예제

---
+++ KoreaITAcademy/Day01/src/Lesson/HelloWorld2.java
@@ -0,0 +1,12 @@
+package Lesson;
+
+public class HelloWorld2 {
+
+	public static void main(String[] args) {
+		String helloWorld = "Hello World~!";
+		
+		System.out.println(helloWorld);
+		
+	}
+
+}
