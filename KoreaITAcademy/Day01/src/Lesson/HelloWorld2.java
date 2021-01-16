95853c4538e75135c4968bab3fc119acf642b54d
Song Myounggyu <skillvirus00@gmail.com>
2021-01-16 22:34:58 +0900

    리눅스에서 커밋 테스트

--- KoreaITAcademy/Day01/src/Lesson/HelloWorld2.java
+++ KoreaITAcademy/Day01/src/Lesson/HelloWorld2.java
@@ -3,7 +3,7 @@ package Lesson;
 public class HelloWorld2 {
 
 	public static void main(String[] args) {
-		String helloWorld = "Hello World~!";
+		String helloWorld = "Hello World~!!";
 		System.out.println(helloWorld);
 	}
 }
