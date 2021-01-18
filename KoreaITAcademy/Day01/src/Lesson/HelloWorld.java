9b45038b63ec74c526f335aabb284569f0c6179c
MG Song <mg-song@poongsan.co.kr>
2021-01-18 10:31:09 +0900

    패키지명 변경

--- KoreaITAcademy/Day01/src/Lesson/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package lesson;
-
-public class HelloWorld {
-	public static void main(String[] args) {
-		System.out.println("Hello World~!");
-	}
-}
--- KoreaITAcademy/Day01/src/lesson/HelloWorld2.java
+++
@@ -1,8 +0,0 @@
-package lesson;
-
-public class HelloWorld2 {
-	public static void main(String[] args) {
-		String helloWorld = "Hello World~~~!";
-		System.out.println(helloWorld);
-	}
-}
