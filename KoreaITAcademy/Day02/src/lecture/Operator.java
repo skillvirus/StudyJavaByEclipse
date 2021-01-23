01ae46cce24836212f9de9de6c8d1c2bdf2f859b
Song Myounggyu <skillvirus00@gmail.com>
2021-01-23 22:35:37 +0900

    1주차 강의용 코드

---
+++ KoreaITAcademy/Day02/src/lecture/Operator.java
@@ -0,0 +1,14 @@
+package lecture;
+
+public class Operator {
+
+	public static void main(String[] args) {
+		int a  = 10;
+		
+		int b = a + 10; //10
+		int c = b - 5; //5
+		
+		System.out.println("변수 a는" + a);
+	}
+
+}
---
+++ KoreaITAcademy/Day02/src/lecture/Print.java
@@ -0,0 +1,14 @@
+package lecture;
+
+public class Print {
+	public static void main(String[] args) {
+		System.out.print("가나");
+		System.out.println("다라");
+		
+		System.out.write(65);
+		System.out.flush();
+		
+		System.out.println();
+		System.err.println("err_line 12");
+	}
+}
