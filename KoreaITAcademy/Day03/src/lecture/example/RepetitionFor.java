7d9b0ef7e7aa21c4be625e857b20127ff5c872cb
MG Song <mg-song@poongsan.co.kr>
2021-01-26 17:33:50 +0900

    조건문, 반복문 예제

---
+++ KoreaITAcademy/Day03/src/lecture/example/RepetitionFor.java
@@ -0,0 +1,14 @@
+package lecture.example;
+
+public class RepetitionFor {
+
+	public static void main(String[] args) {
+		
+		for (int loopCount1 = 2; loopCount1 <= 9; loopCount1++) {
+			for(int loopCount2 = 1; loopCount2 <= 9; loopCount2++) {
+				System.out.println(loopCount1 + " X " + loopCount2 + " = " + loopCount1 * loopCount2);
+			}
+			System.out.println();
+		}
+	}
+}
