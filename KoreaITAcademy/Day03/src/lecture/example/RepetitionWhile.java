13047d6c691cc64ce0e569d0d83b7cbf6a6d3184
Song Myounggyu <skillvirus00@gmail.com>
2021-01-27 06:29:33 +0900

    피라미드 만들기, 반복문(while, dowhile) 예제 추가

---
+++ KoreaITAcademy/Day03/src/lecture/example/RepetitionWhile.java
@@ -0,0 +1,16 @@
+package lecture.example;
+
+public class RepetitionWhile {
+
+	public static void main(String[] args) {
+		
+		int loopCount = 0;
+		
+		while (loopCount < 10) {
+			
+			System.out.println(loopCount);
+			
+			loopCount++;
+		}
+	}
+}
