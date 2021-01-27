ec5a90e1f27fbeb4a94277a0c1d7e87fa6858a27
MG Song <mg-song@poongsan.co.kr>
2021-01-27 09:09:46 +0900

    while 별찍기 추가

--- KoreaITAcademy/Day03/src/lecture/example/RepetitionWhile.java
+++ KoreaITAcademy/Day03/src/lecture/example/RepetitionWhile.java
@@ -4,13 +4,24 @@ public class RepetitionWhile {
 
 	public static void main(String[] args) {
 		
-		int loopCount = 0;
+		int loopCount1 = 0;
+		int loopCount2 = 0;
+		int loopCount3 = 0;
 		
-		while (loopCount < 10) {
+		while (loopCount1 < 5) {
+			loopCount3 = loopCount1;
+			while (loopCount3 < 5 - 1) {
+				System.out.print(" ");
+				loopCount3++;
+			}
 			
-			System.out.println(loopCount);
-			
-			loopCount++;
+			while (loopCount2 < loopCount1 * 2 + 1) {
+				System.out.print("*");
+				loopCount2++;
+			}
+			System.out.println();
+			loopCount2 = 0;
+			loopCount1++;
 		}
 	}
 }
