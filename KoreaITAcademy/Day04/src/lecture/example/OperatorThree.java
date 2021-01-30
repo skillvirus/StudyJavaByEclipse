3020d95eef753bdee88061aa07e42e17f389397c
Song Myounggyu <skillvirus00@gmail.com>
2021-01-31 08:19:25 +0900

    삼항연산자 추가

---
+++ KoreaITAcademy/Day04/src/lecture/example/OperatorThree.java
@@ -0,0 +1,13 @@
+package lecture.example;
+
+public class OperatorThree {
+
+	public static void main(String[] args) {
+		boolean a = true;
+		
+		int b = (a ? 1 : 0);
+		
+		System.out.println("삼항연산자의 결과값은 " + b + "입니다.");
+	}
+
+}
