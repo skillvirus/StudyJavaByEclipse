069677e250b28bd4066e1702087a5c052d8b36e8
Song Myounggyu <skillvirus00@gmail.com>
2021-02-16 22:15:37 +0900

    WindowBuilder 테스트

---
+++ KoreaITAcademy/Day07/src/lecture/example/WindowBuilderTest.java
@@ -0,0 +1,43 @@
+package lecture.example;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class WindowBuilderTest {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					WindowBuilderTest window = new WindowBuilderTest();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public WindowBuilderTest() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
