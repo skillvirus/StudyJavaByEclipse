1334fbf38272f90ad677b405dcf8e259425f73a5
MG Song <mg-song@poongsan.co.kr>
2021-02-24 17:05:41 +0900

    도서대여관리 프로그램 프로젝트명 변경

---
+++ KoreaITAcademy/BookManagementV03GUIWithSQLite/src/book/controler/BookInOut.java
@@ -0,0 +1,79 @@
+package book.controler;
+
+import book.model.DataHelper;
+
+public class BookInOut {
+
+	private String userID;
+	private String UserName;
+	private String bookID;
+	private String bookName;
+	private String inOutType;
+	private String inOutDate;
+	
+	public String getUserID() {
+		return userID;
+	}
+	
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	
+	public String getUserName() {
+		return UserName;
+	}
+	
+	public void setUserName(String userName) {
+		UserName = userName;
+	}
+	
+	public String getBookID() {
+		return bookID;
+	}
+	
+	public void setBookID(String bookID) {
+		this.bookID = bookID;
+	}
+	
+	public String getBookName() {
+		return bookName;
+	}
+	
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+	
+	public String getInOutType() {
+		return inOutType;
+	}
+	
+	public void setInOutType(String inOutType) {
+		this.inOutType = inOutType;
+	}
+	
+	public String getInOutDate() {
+		return inOutDate;
+	}
+	
+	public void setInOutDate(String inOutDate) {
+		this.inOutDate = inOutDate;
+	}
+	
+	DataHelper dataHelper = new DataHelper();
+	
+	public void incomingInOutInfo(BookInOut bookInOut) {
+		dataHelper.insertData(bookInOut);
+	}
+	
+	public void outgoingInOutInfo(BookInOut bookInOut) {
+		dataHelper.insertData(bookInOut);
+	}
+	
+	public void selectInOutInfo(BookInOut bookInOut) {
+		dataHelper.selectData(bookInOut);
+	}
+	
+	public void selectInOutAllInfo(BookInOut bookInOut) {
+		dataHelper.selectAllData(bookInOut);
+	}
+}
---
+++ KoreaITAcademy/BookManagementV03GUIWithSQLite/src/book/view/Main.java
@@ -0,0 +1,9 @@
+package book.view;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Viewer viewer = new Viewer();
+		viewer.start();
+	}
+}
