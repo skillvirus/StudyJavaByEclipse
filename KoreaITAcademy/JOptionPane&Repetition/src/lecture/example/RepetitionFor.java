59929b4bb34692fc814cc54326f739b632799e3d
MG Song <mg-song@poongsan.co.kr>
2021-05-29 21:35:52 +0900

    예제 파일 추가
     - README.md : PPT링크
     - For문 예제 추가

--- KoreaITAcademy/JOptionPane&Repetition/src/lecture/example/RepetitionFor.java
+++ KoreaITAcademy/JOptionPane&Repetition/src/lecture/example/RepetitionFor.java
@@ -3,12 +3,13 @@ package lecture.example;
 public class RepetitionFor {
 
 	public static void main(String[] args) {
+		int toNum = 20;
+		int sum = 0;
 		
-		for (int loopCount1 = 2; loopCount1 <= 9; loopCount1++) {
-			for (int loopCount2 = 1; loopCount2 <= 9; loopCount2++) {
-				System.out.println(loopCount1 + " X " + loopCount2 + " = " + loopCount1 * loopCount2);
-			}
-			System.out.println();
+		for(int i = 0; i <= toNum; i++) {
+			sum += i;
 		}
+		
+		System.out.println("1 부터 " + toNum + " 까지의 합 : " + sum);
 	}
 }
