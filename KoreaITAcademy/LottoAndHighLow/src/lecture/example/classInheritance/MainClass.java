d4c0b99d4e0415a352d68c7de12b28306cb1c789
MG Song <mg-song@poongsan.co.kr>
2021-06-13 06:13:20 +0900

    상속예제 구현중

---
+++ KoreaITAcademy/LottoAndHighLow/src/lecture/example/classInheritance/MainClass.java
@@ -0,0 +1,28 @@
+package lecture.example.classInheritance;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		Computer computer1 = new Computer("가족공용컴퓨터", "거실"); //매개변수가 있는 생성자 이용
+		computer1.turnOn();
+		computer1.turnOff();
+		
+		Refrigerator refrigerator1 = new Refrigerator(); //기본 생성자 이용
+		refrigerator1.setName("공용냉장고");
+		refrigerator1.setLocation("거실");
+		refrigerator1.turnOn();
+		refrigerator1.turnOff();
+		
+		Television television1 = new Television();
+		television1.setName("공용TV");
+		television1.setLocation("거실");
+		television1.turnOn();
+		television1.turnOff();
+		System.out.println(television1.getName() + "는 " + television1.getLocation() + "에 있습니다.");
+		
+		AISpeaker genie = new AISpeaker();
+		genie.turnOn("지니");
+		AISpeaker.reply(); //객체를 생성하지 않고 reply메소드 호출
+	}
+}
---
+++ KoreaITAcademy/LottoAndHighLow/src/lecture/example/classInheritance/Refrigerator.java
@@ -0,0 +1,39 @@
+package lecture.example.classInheritance;
+
+public class Refrigerator {
+	private String name;
+	private String location;
+	
+	public Refrigerator() {
+		
+	}
+	
+	public Refrigerator(String name, String location) {
+		this.name = name;
+		this.location = location;
+	}
+	
+	public void turnOn() {
+		System.out.println(name + " 전원 ON");
+	}
+	
+	public void turnOff() {
+		System.out.println(name + " 전원 OFF");
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+}
