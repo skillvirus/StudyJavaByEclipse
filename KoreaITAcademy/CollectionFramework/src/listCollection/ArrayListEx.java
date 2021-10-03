a8ca8513bac47651701cefe9c38b130833b4e634
SongMG-LecVPC <skillvirus@kakao.com>
2021-10-03 15:55:54 +0900

    Collection Framework 주석 오타 수정

--- KoreaITAcademy/CollectionFramework/src/listCollection/ArrayListEx.java
+++ KoreaITAcademy/CollectionFramework/src/listCollection/ArrayListEx.java
@@ -2,11 +2,12 @@ package listCollection;
 
 import java.util.ArrayList;
 
-/*컬렉션 프레임워크는 배열의 단점을 보완하기 위해 설계됨
+/*
+※컬렉션 프레임워크는 배열의 단점을 보완하기 위해 설계됨
 1. List, Set, Map 가 있음 
-2. List : 순서 유지, 중복 허용(ArrayList, Vector 등
+2. List : 순서 유지, 중복 허용(ArrayList, Vector 등)
 3. Set : 순서 유지 안함, 중복 불가 (HashSet, TreeSet 등)
-4. Mat : Key와 Value로 구성된 데이터 집합(자료형), 순서는 유지 안함, 키는 중복 불가, 값은 중복 허용 (HashMap, TreeMap 등)
+4. Map : Key와 Value로 구성된 데이터 집합(자료형), 순서는 유지 안함, 키는 중복 불가, 값은 중복 허용 (HashMap, TreeMap 등)
 */
 
 public class ArrayListEx {
