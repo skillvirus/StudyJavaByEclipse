83082a35acd8def36ecc50b1c8a437c6fd689320
SongMG-PC <mg-song@poongsan.co.kr>
2021-11-09 12:28:16 +0900

    메서드 예제 보완

--- KoreaITAcademy/LottoAndHighLow/src/lecture/example/methodEx/MethodTest.java
+++ KoreaITAcademy/LottoAndHighLow/src/lecture/example/methodEx/MethodTest.java
@@ -19,13 +19,62 @@ public class MethodTest {
 //		
 //		System.out.println(num3 + num4);
 		
-		//메서드 학습
-		sum(3, 10);
-		sum(10, 20);
+//		public static void sum(int num1, int num2) {
+//		int num3 = num1 + num2;
+//		System.out.println(num3);
+		
+		//클래스, 메서드 학습
+		MethodEx me = new MethodEx();
+		
+		System.out.print("1. 반환형, 매개변수 둘 다 없는 메서드 : "); 
+		me.sum0();
+		
+		System.out.print("2. 반환형은 없고, 매개변수(정수형)는 있는 메서드 : ");
+		me.sum1(3, 10);
+		
+		System.out.print("3. 반환형(정수형), 매개변수(정수형) 둘 다 있는 메서드 : ");
+		System.out.println(me.sum2(10, 20));
+		
+		System.out.print("4. 반환형(문자열형), 매개변수(정수형) 둘 다 있는 메서드 : ");
+		System.out.println(me.sum3(10, 20));
+		
+		System.out.print("5. 반환형(실수형), 매개변수(정수형) 둘 다 있는 메서드 : ");
+		System.out.println(me.sum4(10, 20));
+		
+		System.out.print("6. 반환형(문자열형), 매개변수(문자열형) 둘 다 있는 메서드 : ");
+		System.out.println(me.sum5("송", "명규"));
+	}
+}
+
+class MethodEx {
+
+	//1.반환형, 매개변수 둘 다 없는 메서드
+	public void sum0() {
+		System.out.println(10 + 5);
+	}
+
+	//2. 반환형은 없고, 매개변수(정수형)는 있는 메서드
+	public void sum1(int num1, int num2) {
+		System.out.println(num1 + num2);
 	}
 	
-	public static void sum(int num1, int num2) {
-		int num3 = num1 + num2;
-		System.out.println(num3);
+	//3. 반환형(정수형), 매개변수(정수형) 둘 다 있는 메서드
+	public int sum2(int num1, int num2) {
+		return num1 + num2;
+	}
+	
+	//4. 반환형(문자열형), 매개변수(정수형) 둘 다 있는 메서드
+	public String sum3(int num1, int num2) {
+		return "\"" + Integer.toString(num1 + num2) + "\"";
+	}
+	
+	//5. 반환형(실수형), 매개변수(정수형) 둘 다 있는 메서드
+	public double sum4(int num1, int num2) {
+		return num1 + num2;
+	}
+	
+	//6. 반환형(문자열형), 매개변수(문자열형) 둘 다 있는 메서드
+	public String sum5(String str1, String str2) {
+		return str1 + str2;
 	}
 }
