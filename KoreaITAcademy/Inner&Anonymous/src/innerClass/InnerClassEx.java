98847711e0364016778f6e45cc77c13c7c357f64
SongMG-DevPC <skillvirus@kakao.com>
2021-11-28 10:51:52 +0900

    주석 제거

--- KoreaITAcademy/Inner&Anonymous/src/innerClass/InnerClassEx.java
+++ KoreaITAcademy/Inner&Anonymous/src/innerClass/InnerClassEx.java
@@ -2,10 +2,6 @@ package innerClass;
 
 public class InnerClassEx {
 
-/*
-	주로 Swing, AWT 같은 GUI 프로그래밍 이벤트에서 사용
-*/
-	
 	public static void main(String[] args) {
 		System.out.println(InstanceInner.CONST);
 		System.out.println(StaticInner.cv);
